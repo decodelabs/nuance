c967d05fa063743752ad3bede9712b696b7e8e0f
Tom Wright <tom@inflatablecookie.com>
2025-06-06 12:54:24 +0000

    Added Veneer PluginWrapper custom Entity
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Entity/NativeObject/DecodeLabs/Veneer/Plugin/Wrapper.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @package Nuance
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Nuance\Entity\NativeObject\DecodeLabs\Veneer\Plugin;
+
+use DecodeLabs\Nuance\Entity\NativeObject;
+use DecodeLabs\Veneer\Plugin\Wrapper as WrapperObject;
+
+class Wrapper extends NativeObject
+{
+    public function __construct(
+        // @phpstan-ignore-next-line
+        WrapperObject $wrapper,
+    ) {
+        parent::__construct($wrapper);
+
+        $this->displayName = '@PluginWrapper';
+        // @phpstan-ignore-next-line
+        $this->value = $wrapper->getVeneerPlugin();
+    }
+}
